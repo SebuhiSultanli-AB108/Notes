2f6b04924ab9a515aa7946b85339b47dfee7b3d8
Sabuhi <sabuhies-ab108@code.edu.az>
2024-10-08 10:40:00 +0400

    10/8/2024

---
+++ 1.cs
@@ -0,0 +1,82 @@
+﻿#region Task1
+/*--> Verilmiş natural ədədlərdən ibarət olan array daxilində cüt elementlərin sayının tapılması. <--*/
+//int[] nums = {1, 5, 46, 85, 77, 12545456, 2112, 99};
+//int count = 0;
+//for (int i = 0; i< nums.Length; i++)
+//{
+//    if (nums[i] % 2 == 0) count++;
+//}
+//Console.WriteLine(count);
+#endregion
+
+
+#region Task2
+/*--> Verilmiş natural ədədlərdən ibarət olan array daxilində ən böyük tək elementin tapılması. <--*/
+//int[] nums = {1, 5, 46, 85, 77, 12545456, 2112, 99};
+//int cash = 0;
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] % 2 == 1 && nums[i] > cash) cash = nums[i];
+//}
+//Console.WriteLine(cash);
+#endregion
+
+
+#region Task3
+/*--> Verilmiş array-ın elementlərinin tərsinə çevrilməsi. <--*/
+//int[] nums = { 1, 5, 46, 85, 77, 12545456, 2112, 99 };
+//int[] newNums = new int[nums.Length];
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    newNums[i] = nums[nums.Length - i - 1];
+//}
+
+//for (int i = 0; i < newNums.Length; i++)
+//{
+//    Console.WriteLine(newNums[i]);
+//}
+#endregion
+
+
+#region Task4
+/*--> Verilmiş array-ın elementlərinin sayının tapılması. <--*/
+//int[] nums = { 1, 5, 46, 85, 77, 12545456, 2112, 99 };
+//int count = 0;
+//foreach (int num in nums)
+//{
+//    count++;
+//}
+//Console.WriteLine(count);
+#endregion
+
+
+#region Task5
+/*--> Verilmiş elementin array-in daxilində yer alıb-almaması məlumatının əldə edilməsi. <--*/
+//int[] nums = { 1, 5, 46, 85, 77, 12545456, 2112, 99 };
+//int target = 77;
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] == target)
+//    {
+//        Console.WriteLine("got it! " + " It was at " + i + ". index");
+//        break;
+//    }
+//}
+//Console.WriteLine("can't find it!");
+#endregion
+
+
+#region Task6
+/*--> Verilmiş element array daxilində yer alırsa neçə dəfə təkrarlandığı məlumatının əldə edilməsi. <--*/
+//int[] nums = { 1, 5, 46, 85, 77, 12545456, 2112, 99, 77, 77};
+//int target = 77;
+//int count = 0;
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] == target)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+#endregion
---
+++ 2.cs
@@ -0,0 +1,97 @@
+﻿#region Task1
+//int num = int.Parse(Console.ReadLine());
+//int fact = 1;
+
+//if (num > 1)
+//{
+//    for (int i = 1; i <= num; i++)
+//    {
+//        fact *= i;
+//    }
+//}
+//else if (num < 0)
+//    Console.WriteLine("Give me a number higher then 0 bro !!!");
+//else
+//{
+//    Console.WriteLine(1);
+//}
+//Console.WriteLine(fact);
+
+#endregion
+
+#region Task2
+//int x = 0;
+//int y = x + 1;
+//int cash = 0;
+//while (x < 50000)
+//{
+//    Console.WriteLine(x);
+//    cash = y;
+//    y = x + y;
+//    x = cash;
+//}
+
+#endregion
+
+#region Task3
+//string text = Console.ReadLine();
+//int count = 0;
+
+//for (int i = 0; i < text.Length; i++)
+//{
+//    if (text[i] == 'a')
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+#endregion
+
+#region Task4
+//string text = Console.ReadLine();
+//string target = Console.ReadLine();
+//int count = 0;
+//bool foundIt=false;
+
+//for (int i = 0; i < text.Length; i++)
+//{
+//    if (text[i] == target[count])
+//    {
+//        count++;
+//        if (count == target.Length - 1)
+//        {
+//            foundIt = true;
+//            break;
+//        }
+//    }
+//    else
+//    {
+//        count = 0;
+//    }
+//}
+//if (foundIt) Console.WriteLine("Found it !!!");
+//else Console.WriteLine("There is no " + target  + " in the sentence !!!");
+#endregion
+
+#region Task5
+int[] arr = { 8, 1, 46, 6, 2, 5, 3, 25, };
+int num = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    for (int j = 0; j < arr.Length; j++)
+    {
+        if (arr[i] < arr[j])
+        {
+            num = arr[i];
+            arr[i] = arr[j];
+            arr[j] = num;
+        }
+    }
+}
+
+foreach (int item in arr)
+{
+    Console.WriteLine(item);
+}
+#endregion
