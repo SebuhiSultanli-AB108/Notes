90c5613e7fbabe97699e71df82775984284e7869
Sabuhi <sabuhies-ap118@code.edu.az>
2024-10-11 19:42:57 +0400

    removed unnececery comments

--- 2.cs
+++ 2.cs
@@ -1,182 +1,182 @@
 ﻿#region Task1
-//int num = int.Parse(Console.ReadLine());
-//int fact = 1;
+int num = int.Parse(Console.ReadLine());
+int fact = 1;
 
-//if (num > 1)
-//{
-//    for (int i = 1; i <= num; i++)
-//    {
-//        fact *= i;
-//    }
-//}
-//else if (num < 0)
-//    Console.WriteLine("Give me a number higher then 0 bro !!!");
-//else
-//{
-//    Console.WriteLine(1);
-//}
-//Console.WriteLine(fact);
+if (num > 1)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        fact *= i;
+    }
+}
+else if (num < 0)
+    Console.WriteLine("Give me a number higher then 0 bro !!!");
+else
+{
+    Console.WriteLine(1);
+}
+Console.WriteLine(fact);
 
 #endregion
 
 #region Task2
-//int x = 0;
-//int y = x + 1;
-//int cash = 0;
-//while (x < 50000)
-//{
-//    Console.WriteLine(x);
-//    cash = y;
-//    y = x + y;
-//    x = cash;
-//}
+int x = 0;
+int y = x + 1;
+int cash = 0;
+while (x < 50000)
+{
+    Console.WriteLine(x);
+    cash = y;
+    y = x + y;
+    x = cash;
+}
 
 #endregion
 
 #region Task3
-//string text = Console.ReadLine();
-//int count = 0;
+string text = Console.ReadLine();
+int count = 0;
 
-//for (int i = 0; i < text.Length; i++)
-//{
-//    if (text[i] == 'a')
-//    {
-//        count++;
-//    }
-//}
-//Console.WriteLine(count);
+for (int i = 0; i < text.Length; i++)
+{
+    if (text[i] == 'a')
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
 #endregion
 
 #region Task4
-//string text = Console.ReadLine();
-//string target = Console.ReadLine();
-//int count = 0;
-//bool foundIt = false;
+string text = Console.ReadLine();
+string target = Console.ReadLine();
+int count = 0;
+bool foundIt = false;
 
-//for (int i = 0; i < text.Length; i++)
-//{
-//    if (text[i] == target[count])
-//    {
-//        count++;
-//        if (count == target.Length)
-//        {
-//            foundIt = true;
-//            break;
-//        }
-//    }
-//    else
-//    {
-//        if (count > 0) i-= count;
-//        count = 0;
+for (int i = 0; i < text.Length; i++)
+{
+    if (text[i] == target[count])
+    {
+        count++;
+        if (count == target.Length)
+        {
+            foundIt = true;
+            break;
+        }
+    }
+    else
+    {
+        if (count > 0) i -= count;
+        count = 0;
 
-//    }
-//}
-//if (foundIt) Console.WriteLine("Found it !!!");
-//else Console.WriteLine("There is no " + target + " in the sentence !!!");
+    }
+}
+if (foundIt) Console.WriteLine("Found it !!!");
+else Console.WriteLine("There is no " + target + " in the sentence !!!");
 #endregion
 
 #region Task5
-//int[] arr = { 8, 1, 46, 6, 2, 5, 3, 25, };
-//int num = 0;
+int[] arr = { 8, 1, 46, 6, 2, 5, 3, 25, };
+int num = 0;
 
-//for (int i = 0; i < arr.Length; i++)
-//{
-//    for (int j = 0; j < arr.Length; j++)
-//    {
-//        if (arr[i] < arr[j])
-//        {
-//            num = arr[i];
-//            arr[i] = arr[j];
-//            arr[j] = num;
-//        }
-//    }
-//}
+for (int i = 0; i < arr.Length; i++)
+{
+    for (int j = 0; j < arr.Length; j++)
+    {
+        if (arr[i] < arr[j])
+        {
+            num = arr[i];
+            arr[i] = arr[j];
+            arr[j] = num;
+        }
+    }
+}
 
-//foreach (int item in arr)
-//{
-//    Console.WriteLine(item);
-//}
+foreach (int item in arr)
+{
+    Console.WriteLine(item);
+}
 #endregion
 
 #region Task6
-//int num = int.Parse(Console.ReadLine());
-//int numForLenght = num;
-//int numLenght = 0;
+int num = int.Parse(Console.ReadLine());
+int numForLenght = num;
+int numLenght = 0;
 
-//for (int i = 0; 0 < numForLenght; i++)
-//{
-//    numLenght++;
-//    numForLenght /= 10;
-//}
+for (int i = 0; 0 < numForLenght; i++)
+{
+    numLenght++;
+    numForLenght /= 10;
+}
 
-//int[] arr = new int[numLenght];
-//int mem = 0;
+int[] arr = new int[numLenght];
+int mem = 0;
 
-//for (int i = arr.Length - 1; i >= 0; i--)
-//{
-//    arr[i] = num % 10;
-//    num /= 10;
-//}
+for (int i = arr.Length - 1; i >= 0; i--)
+{
+    arr[i] = num % 10;
+    num /= 10;
+}
 
-//for (int i = 0; i < arr.Length; i++)
-//{
-//    for (int j = 0; j < arr.Length; j++)
-//    {
-//        if (arr[i] > arr[j])
-//        {
-//            mem = arr[i];
-//            arr[i] = arr[j];
-//            arr[j] = mem;
-//        }
-//    }
-//}
+for (int i = 0; i < arr.Length; i++)
+{
+    for (int j = 0; j < arr.Length; j++)
+    {
+        if (arr[i] > arr[j])
+        {
+            mem = arr[i];
+            arr[i] = arr[j];
+            arr[j] = mem;
+        }
+    }
+}
 
-//int final = 0;
-//foreach (int i in arr)
-//{
-//    final *= 10;
-//    final += i;
-//}
+int final = 0;
+foreach (int i in arr)
+{
+    final *= 10;
+    final += i;
+}
 
-//Console.WriteLine(final);
+Console.WriteLine(final);
 #endregion
 
 #region task 7
 
-//int[] arrOne = { 1, 5, 7, 6, 4, 2, 63, 74, 11 };
-//int[] arrTwo = { 5, 7, 56, 4, 5, 7, 1, 5, 4 };
+int[] arrOne = { 1, 5, 7, 6, 4, 2, 63, 74, 11 };
+int[] arrTwo = { 5, 7, 56, 4, 5, 7, 1, 5, 4 };
 
-//bool isEqual = false;
+bool isEqual = false;
 
-//for (int i = 0; i < arrOne.Length; i++)
-//{
-//    for (int j = 0; j < arrTwo.Length; j++)
-//    {
-//        if (arrOne[i] == arrTwo[j])
-//        {
-//            isEqual = true;
-//            break;
-//        }
-//    }
-//    if (!isEqual) Console.WriteLine(arrOne[i]);
-//    isEqual = false;
-//}
+for (int i = 0; i < arrOne.Length; i++)
+{
+    for (int j = 0; j < arrTwo.Length; j++)
+    {
+        if (arrOne[i] == arrTwo[j])
+        {
+            isEqual = true;
+            break;
+        }
+    }
+    if (!isEqual) Console.WriteLine(arrOne[i]);
+    isEqual = false;
+}
 #endregion
 
 #region Task 8
-//int[] arr = { 1, 5, 7, 6, 4, 2, 6, 11 };
+int[] arr = { 1, 5, 7, 6, 4, 2, 6, 11 };
 
-//int evenSum = 0;
-//int oddSum = 0;
+int evenSum = 0;
+int oddSum = 0;
 
-//for (int i = 0; i < arr.Length; i++)
-//{
-//    if(i % 2 == 1) oddSum += arr[i];
-//    else evenSum += arr[i];
-//}
+for (int i = 0; i < arr.Length; i++)
+{
+    if (i % 2 == 1) oddSum += arr[i];
+    else evenSum += arr[i];
+}
 
-//Console.WriteLine(evenSum);
-//Console.WriteLine("----------------");
-//Console.WriteLine(oddSum);
+Console.WriteLine(evenSum);
+Console.WriteLine("----------------");
+Console.WriteLine(oddSum);
 
 #endregion
--- 3.cs
+++ 3.cs
@@ -1,92 +1,92 @@
 ﻿#region Task1
-//string str = Console.ReadLine();
-//char[] arr = new char[str.Length];
+string str = Console.ReadLine();
+char[] arr = new char[str.Length];
 
-//for (int i = 0; i < str.Length; i++)
-//{
-//    arr[i] = str[str.Length - i - 1];
-//}
+for (int i = 0; i < str.Length; i++)
+{
+    arr[i] = str[str.Length - i - 1];
+}
 
-//foreach (char c in arr) Console.WriteLine(c);
+foreach (char c in arr) Console.WriteLine(c);
 #endregion
 
 #region Task2
-//int num = int.Parse(Console.ReadLine());
-//int sum = 0;
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
 
-//for (int i = 1; i < num; i++)
-//{
-//    if (num%i == 0) sum += i;
-//}
+for (int i = 1; i < num; i++)
+{
+    if (num % i == 0) sum += i;
+}
 
-//if (sum == num) Console.WriteLine("Perfect");
-//else Console.WriteLine("Not Perfect");
+if (sum == num) Console.WriteLine("Perfect");
+else Console.WriteLine("Not Perfect");
 #endregion
 
 #region Task3
-//string sait = "aeuioAEUIO";
-//Console.Write("Uzunluq: ");
-//int len = int.Parse(Console.ReadLine());
-//string[] words = new string[len];
-//int Truecount = 0;
+string sait = "aeuioAEUIO";
+Console.Write("Uzunluq: ");
+int len = int.Parse(Console.ReadLine());
+string[] words = new string[len];
+int Truecount = 0;
 
-//for (int i = 0; i < len; i++)
-//{
-//    words[i] = Console.ReadLine();
-//}
+for (int i = 0; i < len; i++)
+{
+    words[i] = Console.ReadLine();
+}
 
-//foreach (string word in words)
-//{
-//    int count = 0;
-//    Console.WriteLine("------------------------");
-//    Console.WriteLine("Soz: " + word);
-//    foreach (char c in word)
-//    {
-//        for (int i = 0; i < sait.Length; i++)
-//        {
-//            if (sait[i] == c) count++;
-//            break;
-//        }
-//    }
-//    Console.Write("Sait: ");
-//    int guess = int.Parse(Console.ReadLine());
-//    if (guess == count)
-//    {
-//        Console.WriteLine();
-//        Console.WriteLine("True!");
-//        Truecount++;
-//    }
-//    else Console.WriteLine("Flase");
-//}
-//Console.WriteLine(Truecount + "/" + len);
+foreach (string word in words)
+{
+    int count = 0;
+    Console.WriteLine("------------------------");
+    Console.WriteLine("Soz: " + word);
+    foreach (char c in word)
+    {
+        for (int i = 0; i < sait.Length; i++)
+        {
+            if (sait[i] == c) count++;
+            break;
+        }
+    }
+    Console.Write("Sait: ");
+    int guess = int.Parse(Console.ReadLine());
+    if (guess == count)
+    {
+        Console.WriteLine();
+        Console.WriteLine("True!");
+        Truecount++;
+    }
+    else Console.WriteLine("Flase");
+}
+Console.WriteLine(Truecount + "/" + len);
 #endregion
 
 #region Task4
-//int[] arrOne = { 1, 5, 6, 2, 4, 89, 5, 4, 154 };
-//int[] arrTwo = { 4, 45, 56, 68, 89, 6 };
-//int[] newArr = new int[arrOne.Length + arrTwo.Length];
+int[] arrOne = { 1, 5, 6, 2, 4, 89, 5, 4, 154 };
+int[] arrTwo = { 4, 45, 56, 68, 89, 6 };
+int[] newArr = new int[arrOne.Length + arrTwo.Length];
 
-//for (int i = 0; i < arrOne.Length + arrTwo.Length; i++)
-//{
-//    if (i < arrOne.Length) newArr[i] = arrOne[i];
-//    else if (i < arrOne.Length + arrTwo.Length) newArr[i] = arrTwo[i - arrOne.Length];
-//}
+for (int i = 0; i < arrOne.Length + arrTwo.Length; i++)
+{
+    if (i < arrOne.Length) newArr[i] = arrOne[i];
+    else if (i < arrOne.Length + arrTwo.Length) newArr[i] = arrTwo[i - arrOne.Length];
+}
 
-//foreach (int i in newArr) Console.WriteLine(i);
+foreach (int i in newArr) Console.WriteLine(i);
 #endregion
 
 #region Task5
-//int[] arrOne = { 1, 4, 7, 9 };
-//bool sirali = true;
+int[] arrOne = { 1, 4, 7, 9 };
+bool sirali = true;
 
-//for (int x = 0; x < arrOne.Length - 1; x++)
-//{
-//    if (arrOne[x] > arrOne[x + 1])
-//    {
-//        sirali = false;
-//        break;
-//    }
-//}
-//if (sirali) Console.WriteLine("Siralidi");
-//else Console.WriteLine("Sirali deyil");
+for (int x = 0; x < arrOne.Length - 1; x++)
+{
+    if (arrOne[x] > arrOne[x + 1])
+    {
+        sirali = false;
+        break;
+    }
+}
+if (sirali) Console.WriteLine("Siralidi");
+else Console.WriteLine("Sirali deyil");
 #endregion
