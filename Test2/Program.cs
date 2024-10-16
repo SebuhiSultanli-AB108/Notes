97106d2c4859c7c474138d9a3adab0f6e3955ee0
Sabuhi <sabuhies-ab108@code.edu.az>
2024-10-16 13:11:06 +0400

    task 3 aded

---
+++ Test2/Program.cs
@@ -0,0 +1,73 @@
+﻿using Test2.Models;
+
+namespace Test2;
+
+//                     //
+//        /\_/\        //
+//       ( o.o )       //
+//         >^<         //
+//                     //
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("                            ╾━╤デ╦︻ \n");
+        Console.Write("Max Ammo: ");
+        int maxAmmo = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Ammo: ");
+        int ammo = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Mag Fixed Empty Seconds: ");
+        int magFixedMtSec = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Is Auto(true/false): ");
+        bool isAuto = Convert.ToBoolean(Console.ReadLine());
+        Weapon weapon = new Weapon(maxAmmo,ammo,magFixedMtSec,isAuto);
+        Console.WriteLine("0 - Shoot\n" +
+                          "1 - Fire\n" +
+                          "2 - Fire Mode\n" +
+                          "3 - Remaining bullets\n" +
+                          "4 - Info\n" +
+                          "5 - Reload\n" +
+                          "6 - Exit\n");
+
+        while (true)
+        {
+            
+            bool stopTheGame = false;
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            switch (input)
+            {
+                case 0:
+                    weapon.Shoot();
+                    break;
+                case 1:
+                    weapon.Fire();
+                    break;
+                case 2:
+                    Console.Write("Is Auto(true/false): ");
+                    bool Auto = Convert.ToBoolean(Console.ReadLine());
+                    Console.WriteLine(Auto);
+                    weapon.ChangeFireMode(Auto);
+                    Console.WriteLine(weapon.IsAuto);
+                    break;
+                case 3:
+                    weapon.GetRemainBulletCount();
+                    break;
+                case 4:
+                    weapon.MagInfo();
+                    break;
+                case 5:
+                    weapon.Reload();
+                    break;
+                case 6:
+                    stopTheGame = true;
+                    break;
+                default:
+                    Console.WriteLine("Give me a input between 0 and 5 please!");
+                    break;
+            }
+            if (stopTheGame) break;
+        }
+    }
+}
